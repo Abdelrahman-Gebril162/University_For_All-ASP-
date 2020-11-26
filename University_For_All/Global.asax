983e0e86ba63592341f9f21dd107cf38f6eb2f0a
abdo-giprel <abdo.gebril2000@gmail.com>
2020-11-26 18:50:13 +0200

    Add project files.

---
+++ University_For_All/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="University_For_All.MvcApplication" Language="C#" %>
---
+++ University_For_All/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University_For_All.Startup))]
+namespace University_For_All
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
