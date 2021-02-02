45df7f5760972c58bf0a825f4ed70dc436849477
abdo-giprel <abdo.gebril2000@gmail.com>
2021-02-02 20:06:46 +0200

    Finish role Administration

---
+++ University_For_All/ViewModels/CreateRoleViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace University_For_All.ViewModels
+{
+    public class CreateRoleViewModels
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
