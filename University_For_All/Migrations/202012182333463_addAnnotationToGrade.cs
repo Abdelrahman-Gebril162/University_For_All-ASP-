f1a21506654aeaf6b128b438ac4be2728736e54d
abdo-giprel <abdo.gebril2000@gmail.com>
2021-02-25 21:28:36 +0200

    delete add latter migration file and move it to addAnnotationto grade file

--- University_For_All/Migrations/202012182333463_addAnnotationToGrade.cs
+++ University_For_All/Migrations/202012182333463_addAnnotationToGrade.cs
@@ -7,6 +7,11 @@ namespace University_For_All.Migrations
     {
         public override void Up()
         {
+            Sql("UPDATE Grades SET Latter = 'A' WHERE id = 4");
+            Sql("UPDATE Grades SET Latter = 'B' WHERE id = 3");
+            Sql("UPDATE Grades SET Latter = 'C' WHERE id = 2");
+            Sql("UPDATE Grades SET Latter = 'D' WHERE id = 1");
+            Sql("UPDATE Grades SET Latter = 'F' WHERE id = 5");
             AlterColumn("dbo.Grades", "Latter", c => c.String(nullable: false, maxLength: 1));
         }
         
